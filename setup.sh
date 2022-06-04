12bda41fffe08f531a6ce37207303b7b0beb27e6
root <root@diplom.iuh1thgjkjme1cnr1sorfjf1oa.dx.internal.cloudapp.net>
2022-06-04 12:27:37 +0000

    edit metricbeat

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ STACK_VERSION=8.0.0
 docker pull elasticsearch:$STACK_VERSION
 docker pull kibana:$STACK_VERSION
 docker pull logstash:$STACK_VERSION
+docker pull metricbeat:$STACK_VERSION
 docker pull debian:11
 
 python3 manage.py runserver $1
