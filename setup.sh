bb764b59852bd9c4d2142764dd887eb90560206f
root <root@diplom.iuh1thgjkjme1cnr1sorfjf1oa.dx.internal.cloudapp.net>
2022-06-04 12:36:55 +0000

    update shell

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 STACK_VERSION=8.0.0
 
-docker pull elasticsearch:$STACK_VERSION
-docker pull kibana:$STACK_VERSION
-docker pull logstash:$STACK_VERSION
-docker pull metricbeat:$STACK_VERSION
+docker pull docker.elastic.co/elasticsearch/elasticsearch:$STACK_VERSION
+docker pull docker.elastic.co/kibana/kibana:$STACK_VERSION
+docker pull docker.elastic.co/logstash/logstash:$STACK_VERSION
+docker pull docker.elastic.co/beats/metricbeat:$STACK_VERSION
 docker pull debian:11
 
 python3 manage.py runserver $1
